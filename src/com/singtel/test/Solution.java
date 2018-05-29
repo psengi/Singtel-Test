c49a86c285c2a835b3df0e3b078ccca11688ce8f
prachee <psengineer1108@gmail.com>
2018-05-29 12:38:40 +0800

    question1-4

--- src/com/singtel/test/Solution.java
+++ src/com/singtel/test/Solution.java
@@ -1,45 +1,109 @@
 package com.singtel.test;
 
+class Neighbour {
+	
+	
+}
 class Animal {
 	void walk() {
 		System.out.println("I am walking");
 	}
-}
-
-class Bird extends Animal {
-	void fly() {
-		System.out.println("I am flying");
-	}
-
-	public void sing(String birdName) {
-		switch (birdName) {
-		case "Duck":
-			System.out.println("A duck says: “Quack, quack”");
-			System.out.println("A duck can swim");
+	
+	void sing(String animalName, String neighbour){
+		if(animalName == null) return;
+		switch (animalName) {
+		case "Dog":
+			System.out.println("Woof, woof");
 			
 			break;
-		case "Chicken":
-			System.out.println("A chicken says: “Cluck, cluck”");
-			System.out.println("A chicken cannot fly (assumption: its wings are clipped but ignore that)");
-			
-			break;
-		case "Rooster":
-			System.out.println("A rooster says: “Cock-a-doodle-doo”");
+		case "Cat":
+			System.out.println("Meow");
 			
 			break;
 		default:
 			break;
 		}
 	}
+	
+	
+}
+
+class Bird extends Animal {
+	void fly(String birdName) {
+		switch (birdName) {
+		case "Chicken":
+			System.out.println("A chicken cannot fly (assumption: its wings are clipped but ignore that)");
+			
+			break;
+
+		default:
+			System.out.println("I am flying");
+			break;
+		}
+		
+	}
+	
+	void swim (String birdName){
+		
+		switch (birdName) {
+		case "Duck":
+			System.out.println("A duck can swim");
+			
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	void stayWith(String name){
+		sing(name, null);
+	}
+	
+	public void sing(String birdName, String neighbour) {
+		super.sing(birdName, neighbour);
+		switch (birdName) {
+		case "Duck":
+			System.out.println("Quack, quack");
+			
+			break;
+		case "Chicken":
+			System.out.println("Cluck, cluck");
+			
+			break;
+		case "Rooster":
+			System.out.println("Cock-a-doodle-doo");
+			
+			break;
+		case "Parrot":
+			stayWith(neighbour);
+			
+			break;
+		
+		default:
+			break;
+		}
+		
+		
+	}
+	
+	class Parrot extends Bird {
+		
+	}
+	
+	
 }
 
 public class Solution {
 	public static void main(String[] args) {
 		Bird bird = new Bird();
 		bird.walk();
-		bird.fly();
-		bird.sing("Duck");
-		bird.sing("Chicken");
-		bird.sing("Rooster");
+		bird.fly("Duck");
+		bird.sing("Duck", null);
+		bird.sing("Chicken", null);
+		bird.sing("Rooster", null);
+		bird.sing("Parrot", "Dog");
+		bird.sing("Parrot", "Cat");
+		bird.sing("Parrot", "Rooster");
 	}
 }
