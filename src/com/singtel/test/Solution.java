b16773c28f4571d7339fa45a4ea4d022fdbb0830
prachee <psengineer1108@gmail.com>
2018-05-29 13:26:24 +0800

    Question B.1

--- src/com/singtel/test/Solution.java
+++ src/com/singtel/test/Solution.java
@@ -5,8 +5,17 @@ class Neighbour {
 	
 }
 class Animal {
-	void walk() {
-		System.out.println("I am walking");
+	void walk(String name) {
+		//
+		switch (name) {
+		case "":
+			System.out.println("I am walking");
+			break;
+
+		default:
+			System.out.println(name+" don’t walk.");
+			break;
+		}
 	}
 	
 	void sing(String animalName, String neighbour){
@@ -21,6 +30,7 @@ class Animal {
 			
 			break;
 		default:
+			
 			break;
 		}
 	}
@@ -50,18 +60,25 @@ class Bird extends Animal {
 			System.out.println("A duck can swim");
 			
 			break;
-
+		case "Fishes":
+			System.out.println("Fishes can swim");
+			
+			break;
 		default:
+			System.out.println(birdName+" can't swim.");
 			break;
 		}
 	}
 	
-	void stayWith(String name){
-		sing(name, null);
+	void stayWith(String name, boolean isBird){
+		if (isBird)
+			sing(name, null);
+		else
+			super.sing(name, null);
 	}
 	
 	public void sing(String birdName, String neighbour) {
-		super.sing(birdName, neighbour);
+		//super.sing(birdName, neighbour);
 		switch (birdName) {
 		case "Duck":
 			System.out.println("Quack, quack");
@@ -76,11 +93,13 @@ class Bird extends Animal {
 			
 			break;
 		case "Parrot":
-			stayWith(neighbour);
+			stayWith(neighbour, false);
 			
 			break;
 		
 		default:
+			System.out.println(birdName+" don't sing.");
+			
 			break;
 		}
 		
@@ -97,7 +116,7 @@ class Bird extends Animal {
 public class Solution {
 	public static void main(String[] args) {
 		Bird bird = new Bird();
-		bird.walk();
+		bird.walk("");
 		bird.fly("Duck");
 		bird.sing("Duck", null);
 		bird.sing("Chicken", null);
@@ -105,5 +124,9 @@ public class Solution {
 		bird.sing("Parrot", "Dog");
 		bird.sing("Parrot", "Cat");
 		bird.sing("Parrot", "Rooster");
+		
+		bird.sing("Fishes", null);
+		bird.walk("Fishes");
+		bird.swim("Fishes");
 	}
 }
